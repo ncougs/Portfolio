190ef8dfbf4eab0c2c537880bbc7b93f317dd621
ncougs <ncougan@gmail.com>
2021-09-13 14:49:34 +1000

    Adding email functionality on submit form

--- controller/api/mail-routes.js
+++ controller/api/mail-routes.js
@@ -6,27 +6,31 @@ router.post('/', async (req, res) => {
 
       const { email, name, message } = req.body;
 
-      const emailResponse = `Hi ${name}, thank you for message. I will respond shortly.
+      if (email, name, message) {
+        const emailResponse = `Hi ${name}, thank you for message. I will respond shortly.
 
-Message details:     
-${message}.`;
-
-      const mailOptions = {
-        from: 'ncougan@gmail.com',
-        to: email,
-        cc: 'ncougan@gmail.com',
-        subject: 'Thank you for message',
-        text: emailResponse
+        Message details:     
+        ${message}.`;
+        
+              const mailOptions = {
+                from: 'ncougan@gmail.com',
+                to: email,
+                cc: 'ncougan@gmail.com',
+                subject: 'Thank you for message',
+                text: emailResponse
+              };
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                  if (error) {
+                      console.log(error);
+                  } else {
+                      res.send('Email sent: ' + info.response);
+                  }
+              });
+      }
+      else {
+        res.send('fields missing in request')
       };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-              console.log(error);
-          } else {
-              res.send('Email sent: ' + info.response);
-          }
-      });
-
     }
     catch (err) {
       res.status(400).json(err);
--- public/js/handle-form.js
+++ public/js/handle-form.js
@@ -1,32 +1,64 @@
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+
 const handleFormSubmit = async (event) => {
     event.preventDefault;
 
-    const nameEL = document.querySelector('#name');
-    const emailEL = document.querySelector('#email');
-    const messageEL = document.querySelector('#message');
+    if ($('#contact-form').hasClass('was-validated')) {
+        $('#form-submit').append(`<span id="spinner" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`);
 
-    const name = nameEL.value.trim();
-    const email = emailEL.value.trim();
-    const message = messageEL.value.trim();
-
-    const body = { name, email, message };
-
-    const request = await fetch('/api/mail', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-    if (request.ok) {
-        console.log('email sent successfully');
+        const nameEL = document.querySelector('#name');
+        const emailEL = document.querySelector('#email');
+        const messageEL = document.querySelector('#message');
+    
+        const name = nameEL.value.trim();
+        const email = emailEL.value.trim();
+        const message = messageEL.value.trim();
+    
+        const body = { name, email, message };
+    
+        const request = await fetch('/api/mail', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+          });
+    
+        if (request.ok) {
+            console.log('email sent successfully');
+            $('.toast').toast('show');
+        }
+        else {
+            console.error(request);
+        };
+    
+        nameEL.value = '';
+        emailEL.value = '';
+        messageEL.value = '';
+    
+        $('#spinner').remove();
     }
     else {
-        console.error(request);
-    };
-
-    nameEL.value = '';
-    emailEL.value = '';
-    messageEL.value = '';
+        return;
+    };    
 
 };
 
